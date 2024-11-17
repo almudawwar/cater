36a5a6cdfca1e6b7002df1bb3fe5aa14c6b52b11
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-11-17 19:10:30 +0100

    Fix menu title references

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,8 +1,8 @@
 class Menu < ApplicationRecord
-  attr_accessor :name, :price
+  attr_accessor :title, :price
 
-  validates_presence_of :name, :price
+  validates_presence_of :title, :price
 
   validates_numericality_of :price, greater_than_or_equal_to: 1
-  validates_length_of :name, minimum: 3
+  validates_length_of :title, minimum: 3
 end

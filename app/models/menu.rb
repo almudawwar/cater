951789cbd59115759031c97606e0cf3e64302dc8
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-11-18 19:24:56 +0100

    Add currencies constant to Menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,10 +1,12 @@
 class Menu < ApplicationRecord
+  CURRENCIES = %w[EUR].freeze
+
   validates_presence_of :title, :price, :currency
 
   validates_numericality_of :price, greater_than_or_equal_to: 1
   validates_length_of :title, minimum: 3
 
-  validates :currency, inclusion: { in: %w[EUR] }
+  validates :currency, inclusion: { in: CURRENCIES }
 
   scope :by_title, ->(term) { where("title ilike '%#{term}%'") }
   scope :sorted_price, ->(sort) { order(price: sort) }
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -8,5 +8,5 @@ RSpec.describe Menu, type: :model do
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(1) }
 
   it { should validate_length_of(:title).is_at_least(3) }
-  it { should validate_inclusion_of(:currency).in_array(%w[EUR]) }
+  it { should validate_inclusion_of(:currency).in_array(Menu::CURRENCIES) }
 end

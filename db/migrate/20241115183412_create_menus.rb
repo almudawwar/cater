d4e6bfbfdafc37c0679569b398a6bf3c16ca563c
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-11-18 16:06:58 +0100

    Change price data type

--- db/migrate/20241115183412_create_menus.rb
+++ db/migrate/20241115183412_create_menus.rb
@@ -1,8 +1,8 @@
 class CreateMenus < ActiveRecord::Migration[7.1]
   def change
     create_table :menus do |t|
-      t.string :title
-      t.bigint :price
+      t.string :title, null: false
+      t.decimal :price, precision: 8, scale: 2, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_15_183412) do
   enable_extension "plpgsql"
 
   create_table "menus", force: :cascade do |t|
-    t.string "title"
-    t.bigint "price"
+    t.string "title", null: false
+    t.decimal "price", precision: 8, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

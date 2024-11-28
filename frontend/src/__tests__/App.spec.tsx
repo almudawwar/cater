992dcf9a9492028bc1558a4218426d60ff26344f
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-11-28 18:15:52 +0100

    Add more examples to App specs

--- frontend/src/__tests__/App.spec.tsx
+++ frontend/src/__tests__/App.spec.tsx
@@ -1,16 +1,60 @@
 import { render, screen } from '@testing-library/react'
 import App from "../App"
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import * as fetchMenusHooks from '../hooks/useFetchMenus'
+import userEvent from '@testing-library/user-event'
+import { keyboard } from '@testing-library/user-event/dist/cjs/setup/directApi.js'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-})
-test('demo', () => {
-  render(<QueryClientProvider client={queryClient}><App /></QueryClientProvider>)
-  const title = screen.queryByText('Cater')
-  expect(title).toBeVisible()
+const mockedMenus = [
+  { id: 1, title: 'Test Menu', price: 45, currency: 'EUR' },
+  { id: 2, title: 'Another Menu', price: 33, currency: 'EUR' },
+  { id: 3, title: '333', price: 33, currency: 'EUR' },
+  { id: 4, title: '4444', price: 33, currency: 'EUR' },
+  { id: 5, title: 'Starting 5', price: 33, currency: 'EUR' },
+]
+
+describe('App', () => {
+  const menusSpy = vi.spyOn(fetchMenusHooks, 'useFetchMenus')
+  
+  // Mock IntersectionObserver for infinite scroll purposes
+  beforeEach(() => {
+    const mockIntersectionObserver = vi.fn();
+    mockIntersectionObserver.mockReturnValue({
+      observe: () => null,
+      unobserve: () => null,
+      disconnect: () => null
+    });
+    window.IntersectionObserver = mockIntersectionObserver;
+  });
+
+  it('renders title and components', () => {
+    menusSpy.mockReturnValue({
+      data: { pages: [{menus: mockedMenus, next_page: null}] },
+      isFetching: false,
+      hasNextPage: true,
+    })
+
+    render(<App />)
+    const title = screen.queryByText('Cater')
+    expect(title).toBeVisible()
+    expect(screen.queryByText('Price')).toBeVisible()
+    expect(screen.queryByText('Test Menu')).toBeVisible()
+    expect(screen.queryByText('Another Menu')).toBeVisible()
+  })
+
+  describe('While loading', () => {
+    it('renders LoadingList', () => {
+      menusSpy.mockReturnValue({
+        data: { pages: [{menus: [], next_page: null}] },
+        isFetching: true,
+        hasNextPage: false,
+      })
+
+      render(<App />)
+      expect(screen.queryByText('Loading...')).toBeInTheDocument()
+    })
+  })
+
+  describe('when user scrolls to bottom', () => {
+    it.todo('calls fetchNextPage')
+  })
 })

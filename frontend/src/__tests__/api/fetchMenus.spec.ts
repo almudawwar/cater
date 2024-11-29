7691a81843d66c934e1c7b9714fd2873bf7211ad
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-11-29 14:25:12 +0100

    Add axios-mock-adapter, add specs for fetchMenus api

---
+++ frontend/src/__tests__/api/fetchMenus.spec.ts
@@ -0,0 +1,34 @@
+import axios from "axios"
+import AxiosMockAdapter from 'axios-mock-adapter'
+import { apiUrls } from "../../constants"
+import { fetchMenus } from "../../api/fetchMenus"
+
+describe('#fetchMenus', () => {
+  const mock = new AxiosMockAdapter(axios)
+
+  it('returns menus and next_page', async () => {
+    mock.onGet(apiUrls.base + apiUrls.menus).reply(
+      200,
+      {
+        menus: [],
+        next_page: null
+      }
+    )
+
+    const res = await fetchMenus({})
+
+    expect(res).toEqual({ menus: [], next_page: null })
+  })
+
+  describe('on error', () => {
+    it('throws an error', async () => {
+      mock.onGet(apiUrls.base + apiUrls.menus).networkError()
+
+      try {
+        await fetchMenus({});
+      } catch (error) {
+        expect(error.message).toEqual('Network Error');
+      }
+    })
+  })
+})
